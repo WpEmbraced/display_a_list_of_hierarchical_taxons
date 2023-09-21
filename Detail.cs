b85351328c921a9c462dbfabf5974a0d0d8733d3
WpEmbraced <45814109+WpEmbraced@users.noreply.github.com>
2023-09-21 12:15:07 -0400

    Update Detail.cs

--- Detail.cs
+++ Detail.cs
@@ -8,3 +8,14 @@
         </div>
     }
 }
+
+// Convert the list of taxons into a comma-separated string instead of a list
+
+@if (Model.Item.Fields.Category != null && Model.Item.GetHierarchicalTaxons("Category").Count() > 0) {
+    var taxons = Model.Item.GetHierarchicalTaxons("Category").Where(c => c.Parent != null && c.Parent.Name.Equals("interest-groups")).Select(t => t.Title);
+    if (taxons.Any()) {
+        <div>
+            @string.Join(", ", taxons)
+        </div>
+    }
+}
